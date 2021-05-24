7ae11b7970b34b67b9839b0df3bb7e862849338a
255934 <milan.apegaonkar@ltts.com>
2021-05-24 23:41:35 +0530

    no

--- 3_Implementation/src/MakeUserTable.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-connection = sqlite3.connect("../project.db")
-cursor = connection.cursor()
-user = """CREATE TABLE IF NOT EXISTS
-user(name, age, ph_no, userid, password, aadhaar)"""
-cursor.execute(user)
-connection.commit()
-connection.close()
